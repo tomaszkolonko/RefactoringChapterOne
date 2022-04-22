398fecc79541f9bef39946bb633e9b1cc1916ee3
Tomasz <tz.kolonko@gmail.com>
2022-04-22 14:31:55 +0200

    replaced unnecessary method calls

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -9,7 +9,7 @@ public class Rental {
     }
 
     public int getDaysRented() {
-        return daysRented;
+        return this.daysRented;
     }
 
     public Movie getMovie() {
@@ -18,20 +18,20 @@ public class Rental {
 
     double getAmountToPay() {
         double result = 0.0;
-        switch (getMovie().getPriceCode()) {
+        switch (movie.getPriceCode()) {
             case Movie.REGULAR:
                 result += 2;
-                if (getDaysRented() > 2) {
-                    result += (getDaysRented() - 2) + 1.5;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) + 1.5;
                 }
                 break;
             case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
+                result += daysRented * 3;
                 break;
             case Movie.CHILDREN:
                 result += 1.5;
-                if (getDaysRented() > 3) {
-                    result += (getDaysRented() - 3) * 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
                     break;
                 }
         }
