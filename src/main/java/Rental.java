238fd832f96a00ead1dfb110358bc7d14d7f9940
Tomasz <tz.kolonko@gmail.com>
2022-04-22 15:53:18 +0200

    moved switch statement to where the data lies that it is switching on

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -1,6 +1,6 @@
 public class Rental {
 
-    private Movie movie;
+    Movie movie;
     private int daysRented;
 
     public Rental(Movie movie, int daysRented) {
@@ -16,28 +16,6 @@ public class Rental {
         return movie;
     }
 
-    double getAmountToPay() {
-        double result = 0.0;
-        switch (movie.getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) + 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                    break;
-                }
-        }
-        return result;
-    }
-
     public int getFrequentRenterPoints() {
         int frequentRenterPoints = 1;
 
@@ -45,5 +23,9 @@ public class Rental {
         if ((this.movie.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
             frequentRenterPoints++;
         return frequentRenterPoints;
+    }
+
+    public double getAmountToPay() {
+        return movie.getAmountToPay(getDaysRented());
     }
 }
