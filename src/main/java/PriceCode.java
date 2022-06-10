25dc6c8cff8386b52e00a4aedeee352d8927580f
Tomasz Kolonko <thomas.kolonko@sbb.ch>
2022-06-10 13:50:22 +0200

    use classes instead of enum

--- src/main/java/PriceCode.java
+++ src/main/java/PriceCode.java
@@ -1,32 +1,42 @@
-import java.util.function.Function;
+public abstract class PriceCode {
 
-public enum PriceCode {
-    REGULAR(
-        rentedDays -> FrequenterPoints.ONE,
-        rentedDays -> rentedDays > 2 ? 2 + (rentedDays - 2) + 1.5 : 2.0
-    ),
-    NEW_RELEASE(
-        rentedDays -> rentedDays > 1 ? FrequenterPoints.TWO : FrequenterPoints.ONE,
-        rentedDays -> rentedDays * 3.0
-    ),
-    CHILDREN(
-        rentedDays -> FrequenterPoints.ONE,
-        rentedDays -> rentedDays > 3 ? 1.5 + (rentedDays - 3) * 1.5 : 1.5
-    );
+    public static PriceCode REGULAR = new PriceCode() {
+        @Override
+        public FrequenterPoints calculateFrequenterPoints(final Integer integer) {
+            return FrequenterPoints.ONE;
+        }
 
-    final private Function<Integer, FrequenterPoints> frequenterPoints;
-    final private Function<Integer, Double> price;
+        @Override
+        public Double calculatePrice(final Integer rentedDays) {
+            return rentedDays > 2 ? 2 + (rentedDays - 2) + 1.5 : 2.0;
+        }
+    };
 
-    PriceCode(Function<Integer, FrequenterPoints> frequenterPoints, final Function<Integer, Double> price) {
-        this.frequenterPoints = frequenterPoints;
-        this.price = price;
-    }
+    public static PriceCode NEW_RELEASE = new PriceCode() {
+        @Override
+        public FrequenterPoints calculateFrequenterPoints(final Integer rentedDays) {
+            return rentedDays > 1 ? FrequenterPoints.TWO : FrequenterPoints.ONE;
+        }
 
-    public FrequenterPoints calculateFrequenterPoints(final Integer integer) {
-        return frequenterPoints.apply(integer);
-    }
+        @Override
+        public Double calculatePrice(final Integer rentedDays) {
+            return rentedDays * 3.0;
+        }
+    };
 
-    public Double calculatePrice(final Integer integer) {
-        return price.apply(integer);
-    }
+    public static PriceCode CHILDREN = new PriceCode() {
+        @Override
+        public FrequenterPoints calculateFrequenterPoints(final Integer integer) {
+            return FrequenterPoints.ONE;
+        }
+
+        @Override
+        public Double calculatePrice(final Integer rentedDays) {
+            return rentedDays > 3 ? 1.5 + (rentedDays - 3) * 1.5 : 1.5;
+        }
+    };
+
+    public abstract FrequenterPoints calculateFrequenterPoints(final Integer integer);
+
+    public abstract Double calculatePrice(final Integer integer);
 }
